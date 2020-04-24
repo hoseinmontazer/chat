aed0229e7111c7be61a6c7e7539d333d980c6e33
hosein montazer <hosein.montazer.montazer@gmail.com>
2020-04-25 00:11:48 +0430

    Initial commit

---
+++ chaaat.py
@@ -0,0 +1,159 @@
+import asyncio
+import tornado.escape
+import tornado.ioloop
+import tornado.locks
+import tornado.web
+import os.path
+import uuid
+
+from tornado.options import define, options, parse_command_line
+
+
+
+
+
+
+define("port", default=8888, help="run on the given port", type=int)
+define("debug", default=True, help="run in debug mode")
+#####################################
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+
+
+
+
+class MessageBuffer(object):
+    def __init__(self):
+        # cond is notified whenever the message cache is updated
+        self.cond = tornado.locks.Condition()
+        self.cache = []
+        self.cache_size = 200
+
+    def get_messages_since(self, cursor):
+        """Returns a list of messages newer than the given cursor.
+
+        ``cursor`` should be the ``id`` of the last message received.
+        """
+        results = []
+        for msg in reversed(self.cache):
+            if msg["id"] == cursor:
+                break
+            results.append(msg)
+        results.reverse()
+        return results
+
+    def add_message(self, message):
+        self.cache.append(message)
+        if len(self.cache) > self.cache_size:
+            self.cache = self.cache[-self.cache_size :]
+        self.cond.notify_all()
+
+
+# Making this a non-singleton is left as an exercise for the reader.
+global_message_buffer = MessageBuffer()
+
+
+
+class MainHandler(BaseHandler):
+    def get(self):
+        if not self.current_user:
+            self.redirect("/login")
+            return
+        name = tornado.escape.xhtml_escape(self.current_user)
+        #print ("Hello, " + name)
+        self.render("index.html", messages=global_message_buffer.cache)
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        self.render("login.html",)
+    def post(self):
+        self.set_secure_cookie("user", self.get_argument("name"))
+        self.redirect("/")
+
+
+
+########################################
+########################################
+#
+
+
+
+class MessageNewHandler(BaseHandler):
+    """Post a new message to the chat room."""
+    def post(self):
+        name = tornado.escape.xhtml_escape(self.current_user)
+        print ("Hello, " + name)
+        body= self.get_argument("body")
+        if (len(body) == 0):
+            message = {"id": str(uuid.uuid4()), "body": body}
+        else :
+            message = {"id": str(uuid.uuid4()), "body": name+": "+body}
+        # render_string() returns a byte string, which is not supported
+        # in json, so we must convert it to a character string.
+        message["html"] = tornado.escape.to_unicode(
+            self.render_string("message.html", message= message)
+        )
+        if self.get_argument("next", None):
+            self.redirect(self.get_argument("next"))
+        else:
+            #name = tornado.escape.xhtml_escape(self.current_user)
+            self.write(message)
+        global_message_buffer.add_message(message)
+
+
+
+
+
+class MessageUpdatesHandler(tornado.web.RequestHandler):
+    """Long-polling request for new messages.
+
+    Waits until new messages are available before returning anything.
+    """
+
+    async def post(self):
+        cursor = self.get_argument("cursor", None)
+        messages = global_message_buffer.get_messages_since(cursor)
+        while not messages:
+            # Save the Future returned here so we can cancel it in
+            # on_connection_close.
+            self.wait_future = global_message_buffer.cond.wait()
+            try:
+                await self.wait_future
+            except asyncio.CancelledError:
+                return
+            messages = global_message_buffer.get_messages_since(cursor)
+        if self.request.connection.stream.closed():
+            return
+        self.write(dict(messages=messages))
+
+    def on_connection_close(self):
+        self.wait_future.cancel()
+
+
+
+#########################################
+##   main 
+#########################################
+
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([
+    (r"/", MainHandler),
+    (r"/login", LoginHandler),
+    (r"/a/message/new", MessageNewHandler),
+    (r"/a/message/updates", MessageUpdatesHandler),    
+    ], cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        xsrf_cookies=False,
+        debug=options.debug)
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    main()
